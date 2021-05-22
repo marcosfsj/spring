67bbfd9f9b449864d2ea512e595b459092fc8167
marcosfsj <marcosfsj@gmail.com>
2021-05-21 21:19:47 -0400

    more code

--- src/main/java/com/example/demo/entity/AddressEntity.java
+++ src/main/java/com/example/demo/entity/AddressEntity.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
 @Entity(name = "address")
 @Table(name = "address")
 public class AddressEntity {
