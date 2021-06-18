e880851c5bc8227d95e93c51ed83f32215292134
marcosfsj <marcosfsj@gmail.com>
2021-06-18 09:38:55 -0400

    refactor of the entities

--- src/main/java/com/example/demo/entity/PostEntity.java
+++ src/main/java/com/example/demo/entity/PostEntity.java
@@ -1,22 +1,52 @@
 package com.example.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 import javax.persistence.*;
 
 @Setter
 @Getter
 @Builder
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "POST")
 public class PostEntity {
+
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "DESCRIPTION")
     private String description;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private UserEntity user;
+
+    @Override
+    public boolean equals(final Object that) {
+        return this == that
+                || that != null
+                && (PostEntity.class.isAssignableFrom(that.getClass()))
+                && new EqualsBuilder()
+                        .reflectionAppend(this, that)
+                        .setTestRecursive(true)
+                        .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, new RecursiveToStringStyle()).toString();
+    }
+
 }
--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,25 +1,61 @@
 package com.example.demo.entity;
 
 import lombok.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
 @Setter
+@Getter
 @Builder
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "USER")
 public class UserEntity {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "FIRST_NAME")
     private String firstName;
+
+    @Column(name = "LAST_NAME")
     private String lastName;
-    @OneToMany(mappedBy = "user")
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER,
+            orphanRemoval = true,
+            mappedBy = "user")
     private List<PostEntity> posts = new ArrayList<>();
+
+    @Override
+    public boolean equals(final Object that) {
+        return this == that
+                || that != null
+                && (PostEntity.class.isAssignableFrom(that.getClass()))
+                && new EqualsBuilder()
+                .reflectionAppend(this, that)
+                .setTestRecursive(true)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, new RecursiveToStringStyle()).toString();
+    }
+
 }
