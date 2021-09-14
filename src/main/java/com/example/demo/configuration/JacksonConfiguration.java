42746acfbd99d69bfd5f974ab17953700e857c12
marcosfsj <marcosfsj@gmail.com>
2021-09-14 13:55:06 -0400

    some configs

---
+++ src/main/java/com/example/demo/configuration/JacksonConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.demo.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+
+public class JacksonConfiguration {
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public JacksonConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public void configureObjectMapper() {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    }
+
+}
---
+++ src/main/java/com/example/demo/configuration/RestConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.demo.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestOperations;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public RestOperations xThirdPartyRestOperations(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+
+        return new RestTemplateBuilder()
+                .messageConverters(converter)
+                .build();
+    }
+
+}
